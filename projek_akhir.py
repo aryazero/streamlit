3dee0eb64de6141a574cba56cb0228101402f429
aryazero <156088830+aryazero@users.noreply.github.com>
2024-01-09 21:22:56 +0800

    Update projek_akhir.py

--- projek_akhir.py
+++ projek_akhir.py
@@ -12,9 +12,36 @@ if option == 'Home' or option == '':
 elif option == 'Dataframe':
     st.write("""## Dataframe""") #menampilkan judul halaman dataframe
 
-    #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
-    df = pd.DataFrame({
-        'Column 1':[1,2,3,4],
-        'Column 2':[10,12,14,16]
-    })
-    df #menampilkan dataframe
+    df = pd.read_csv('./diabetes_latih.csv')
+
+    X_train = df.values
+    X_train = np.delete(X_train,8, axis=1)
+    
+    y_train = df['Outcome'].values
+    
+    df = pd.read_csv('./diabetes_uji.csv')
+    
+    X_test = df.values
+    X_test = np.delete(X_test,8,axis=1)
+    
+    y_test = df['Outcome'].values
+
+    from sklearn.preprocessing import MinMaxScaler
+    X_train = MinMaxScaler().fit_transform(X_train)
+    X_test = MinMaxScaler().fit_transform(X_test)
+
+    from keras.models import Sequential
+    from keras.layers import Dense
+    
+    model = Sequential()
+    
+    model.add(Dense(12, activation='relu', input_shape=(8,)))
+    
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    
+    model.add(Dense(1, activation='sigmoid'))
+    
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    df.head()
